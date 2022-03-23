cfe88eb823e1a8779431dcabe3f765141a94efc9
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2022-03-23 16:17:06 +0100

    Added api in its own route.

--- index.mjs
+++ index.mjs
@@ -1,75 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import {
-  createTodo,
-  readAllTodos,
-  readTodo,
-  updateTodo,
-  deleteTodo
-} from './repositories/lowdbService.mjs';
+import apiRouter from './routes/apiRouter.mjs';
 
 const app = express();
+
+//Register view engine
+app.set('view engine', 'ejs');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/todos', (req, res) => {
-  try {
-    const todos = readAllTodos();
-    res.status(200).json(todos);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ errorMessage: err.message });
-  }
-});
-
-app.get('/todos/:id', (req, res) => {
-  try {
-    const todoId = req.params.id;
-    const todo = readTodo(todoId);
-    if (!todo) {
-      return res.status(404).json({ message: 'Todo not found' });
-    }
-    res.status(200).json(todo);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ errorMessage: err.message });
-  }
-});
-
-app.post('/todos', async (req, res) => {
-  try {
-    const addedTodo = await createTodo(req.body);
-    res.status(201).json(addedTodo);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ errorMessage: err.message });
-  }
-});
-
-app.put('/todos', async (req, res) => {
-  try {
-    const updatedTodo = await updateTodo(req.body);
-    if (!updatedTodo) {
-      return res.status(404).json({ message: 'Todo not found' });
-    }
-    res.status(204).json(updatedTodo);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ errorMessage: err.message });
-  }
-});
-
-app.delete('/todos/:id', async (req, res) => {
-  try {
-    const todoId = req.params.id;
-    await deleteTodo(todoId);
-    res.status(201).json({ message: `Deleted todo with id: ${todoId}` });
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ errorMessage: err.message });
-  }
-});
+app.use('/api/todos', apiRouter);
 
 const PORT = process.env.PORT || 9999;
 
---
+++ routes/apiRouter.mjs
@@ -0,0 +1,70 @@
+import { Router } from 'express';
+import {
+  createTodo,
+  readAllTodos,
+  readTodo,
+  updateTodo,
+  deleteTodo
+} from '../repositories/lowdbService.mjs';
+
+const apiRouter = Router();
+
+apiRouter.get('/', (req, res) => {
+  try {
+    const todos = readAllTodos();
+    res.status(200).json(todos);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
+apiRouter.get('/:id', (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const todo = readTodo(todoId);
+    if (!todo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+    res.status(200).json(todo);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
+apiRouter.post('/', async (req, res) => {
+  try {
+    const addedTodo = await createTodo(req.body);
+    res.status(201).json(addedTodo);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
+apiRouter.put('/', async (req, res) => {
+  try {
+    const updatedTodo = await updateTodo(req.body);
+    if (!updatedTodo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+    res.status(204).json(updatedTodo);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
+apiRouter.delete('/:id', async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    await deleteTodo(todoId);
+    res.status(201).json({ message: `Deleted todo with id: ${todoId}` });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
+export default apiRouter;
