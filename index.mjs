47ca1d0f05cfd8aa0385e48e0331cb4c7c89cf5c
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2022-03-25 12:35:14 +0100

    Middleware for static files, form data and added route for server rendering.

--- index.mjs
+++ index.mjs
@@ -1,15 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import apiRouter from './routes/apiRouter.mjs';
+import todoRouter from './routes/todoRouter.mjs';
 
 const app = express();
 
 //Register view engine
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/', todoRouter);
 app.use('/api/todos', apiRouter);
 
 const PORT = process.env.PORT || 9999;
